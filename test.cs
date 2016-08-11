63c8828a2e566bb844c97103fa20b89c0fb2c2c5
CxTest <CaptainMouse@mailinator.com>
2016-08-11 16:48:30 +0300

    added new file

---
+++ test.cs
@@ -0,0 +1,26 @@
+ private static string OpenLicense(string path)
+            {
+                //CxLogger.LogError("path = " + path);
+                if (!File.Exists(path))
+                {
+                    Checkmarx.Licensing.Log.Write("license cannot be found at " + path);
+                    throw new Exception("No License");
+                }
+                else
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(path))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                    catch
+                    {
+                        Checkmarx.Licensing.Log.Write("Code -960: check open l");
+                        throw;
+                    }
+                }
+
+
+            }
