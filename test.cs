785c88c625e4f99103c921aa23ba06748bf4693d
checkmarxQA <Roman.Maerov@checkmarx.com>
2020-01-23 14:22:16 +0200

    Update test.cs
    update file

--- test.cs
+++ test.cs
@@ -1,29 +1,30 @@
  private static string OpenLicense(string path)
+ {
+        CxLogger.LogError("path = " + path);
+        if (!File.Exists(path))
+        {
+            Checkmarx.Licensing.Log.Write("license cannot be found at " + path);
+            throw new Exception("No License");
+        }
+        else
+        {
+            try
             {
-                CxLogger.LogError("path = " + path);
-                if (!File.Exists(path))
+                using (StreamReader sr = new StreamReader(path))
                 {
-                    Checkmarx.Licensing.Log.Write("license cannot be found at " + path);
-                    throw new Exception("No License");
+                    int i = 78;
+                    //i--;
+                    // another comment
+                    i--;
+                    return sr.ReadToEnd();
                 }
-                else
-                {
-                    try
-                    {
-                        using (StreamReader sr = new StreamReader(path))
-                        {
-                         int i = 78;
-                         //i--;
-                            return sr.ReadToEnd();
-                        }
-                    }
-                    catch
-                    {
-                        Checkmarx.Licensing.Log.Write("Code -960: check open l");
-                        throw;
-                    }
-                 
-                }
-
-\\cout<<"hello world"<<endl;
             }
+            catch
+            {
+                Checkmarx.Licensing.Log.Write("Code -960: check open l");
+                throw;
+            }
+        }
+
+        \\cout<<"hello world"<<endl;
+}
